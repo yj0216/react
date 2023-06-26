c05d5b401caad36d7cc4a7cdaa83e30b39438554
yj0216 <yeojun0216@naver.com>
2023-06-26 21:13:55 +0900

    react redux 공부

--- context-tutorial/src/components/ColorBox.js
+++ context-tutorial/src/components/ColorBox.js
@@ -14,8 +14,8 @@ export const ColorBox = () => {
           />
           <div
             style={{
-              width: '32px',
-              height: '32px',
+              width: '64px',
+              height: '64px',
               background: state.subcolor,
             }}
           />
---
+++ react-redux-tutorial/src/App.js
@@ -0,0 +1,14 @@
+import { Counter } from './components/Counter';
+import Todos from './components/Todos';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter number={0}/>
+      <hr/>
+      <Todos/>
+    </div>
+  );
+}
+
+export default App;
